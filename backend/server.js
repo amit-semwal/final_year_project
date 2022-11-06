070ce16499bf7f60fb8e265be84c216ab7e3c1f5
amit-semwal <semwalamit510@gmail.com>
2022-11-06 15:12:00 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json()); //to accept json data
+
+app.get('/', (req, res) => {
+    res.send("API is Running Successfully");
+});
+
+app.use('/api/user',userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+
