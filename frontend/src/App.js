7bbe33b2b254af47fa79e1c712f642e17fc5b0e4
amit-semwal <semwalamit510@gmail.com>
2022-11-17 18:55:16 +0530

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import ChatPage from "./Pages/ChatPage";
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Route path="/" component={Homepage} exact />
-      <Route path = "/chats" component={ChatPage} />
+      <Route path="/chats" component={ChatPage} />
     </div>
   );
 }
